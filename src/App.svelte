6dcac73c53897f6a81614c3f68139d7f7d4e2e37
Eirik Vageskar <eirikvageskar@gmail.com>
2020-09-22 11:47:19 +0200

    :recycle: Avoid an initalSnake variable

--- src/App.svelte
+++ src/App.svelte
@@ -18,16 +18,15 @@
   const TICK_TIME = 100;
   const BOARD_DIMENSIONS = { x: 20, y: 20 };
 
-  const initialSnake = [
+  let snake = [
     { x: 4, y: 4 },
     { x: 4, y: 3 },
     { x: 4, y: 2 },
   ];
-  let apple = getNewApplePosition(BOARD_DIMENSIONS, initialSnake);
+  let apple = getNewApplePosition(BOARD_DIMENSIONS, snake);
   let gameOver = false;
   let headDirection = "SOUTH";
   let score = 0;
-  let snake = initialSnake;
   let willGrow = false;
 
   function moveSnake() {
